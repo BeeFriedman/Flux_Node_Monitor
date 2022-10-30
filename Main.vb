28bb4086546bda5deb48dcc2640fbc64d0b3fc14
= <70110787+BeeFriedman@users.noreply.github.com>
2022-10-30 19:45:20 -0400

    added wallet verification function

---
+++ Main.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Net
+
+Module Main
+
+    Sub Main()
+        Console.WriteLine("Enter Wallet Address:")
+        Dim walletAddress = Console.ReadLine()
+        VerifyWallet(walletAddress)
+    End Sub
+
+    Sub VerifyWallet(walletAddress)
+        Dim WebClient As New WebClient
+        Dim result = WebClient.DownloadString("https://api.runonflux.io/daemon/validateaddress?zelcashaddress=" + walletAddress)
+        Console.WriteLine(result)
+        Console.ReadLine()
+    End Sub
+
+End Module
