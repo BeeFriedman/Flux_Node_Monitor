fa297936992c1d355b9b9f41a4466674a12daf90
= <70110787+BeeFriedman@users.noreply.github.com>
2022-12-28 19:52:09 -0500

    added timer

--- Main.vb
+++ Main.vb
@@ -1,15 +1,27 @@
 ﻿Imports System.Net
+Imports System.Timers
 Imports Newtonsoft.Json
 Imports Newtonsoft.Json.Linq
 
 Module Main
+    Private ipList As New List(Of String)
     Private ipFailList As New List(Of String)
 
     Sub Main()
         Console.WriteLine("Enter Wallet Address:")
         Dim walletAddress = Console.ReadLine()
         VerifyWallet(walletAddress)
-        SendEmail(ipFailList)
+        Dim Timer As New Timer
+        Timer.AutoReset = True
+        Timer.Interval = 60000
+        AddHandler Timer.Elapsed, AddressOf tick
+        Timer.Start()
+        Console.ReadKey()
+    End Sub
+
+    Sub tick(ByVal sender As Object, ByVal e As ElapsedEventArgs)
+        Console.WriteLine("Timer Elapsed")
+        Monitor()
     End Sub
 
     Sub VerifyWallet(walletAddress)
@@ -18,18 +30,14 @@ Module Main
         Dim resultObject = JsonConvert.DeserializeObject(Of Dictionary(Of String, Object))(result)
 
         If resultObject.Item("data").Item("isvalid") Then
-            Dim ipList = GetNodeInfo(walletAddress)
-
-            If ipList.Count > 0 Then
-                For Each ip In ipList
-                    GetNodeBenchMarks(ip)
-                Next
-            End If
+            Console.WriteLine("Wallet address confirmed monitoring started!")
+            GetNodeInfo(walletAddress)
+        Else
+            Console.WriteLine("Wrong wallet address program will shut down!")
         End If
-
     End Sub
 
-    Function GetNodeInfo(walletAddress)
+    Sub GetNodeInfo(walletAddress)
         Dim WebClient As New WebClient
         Dim result = WebClient.DownloadString("https://api.runonflux.io/daemon/listzelnodes?filter=" + walletAddress)
         Dim resultObject = JsonConvert.DeserializeObject(Of Dictionary(Of String, Object))(result)
@@ -47,18 +55,31 @@ Module Main
                 End If
             Next
         End If
-        GetNodeInfo = ipList
-    End Function
+    End Sub
 
     Sub GetNodeBenchMarks(ip)
-        Dim WebClient As New WebClient
-        Dim result = WebClient.DownloadString(String.Format("http://{0}/daemon/getbenchmarks", ip))
-        Dim resultObject = JsonConvert.DeserializeObject(Of Dictionary(Of String, Object))(result)
-        Dim benchmarksResult = JToken.Parse(resultObject.Item("data"))
-        Dim status = benchmarksResult.Item("status")
-
-        If status = "failed" Then
+        Try
+            Dim WebClient As New WebClient
+            Dim result = WebClient.DownloadString(String.Format("http://{0}/daemon/getbenchmarks", ip))
+            Dim resultObject = JsonConvert.DeserializeObject(Of Dictionary(Of String, Object))(result)
+            Dim benchmarksResult = JToken.Parse(resultObject.Item("data"))
+            Dim status = benchmarksResult.Item("status")
+            If status = "failed" Then
+                ipFailList.Add(ip)
+            End If
+        Catch
             ipFailList.Add(ip)
+        End Try
+    End Sub
+
+    Sub Monitor()
+        If ipList.Count > 0 Then
+            For Each ip In ipList
+                GetNodeBenchMarks(ip)
+            Next
+        End If
+        If ipFailList.Count > 0 Then
+            SendEmail(ipFailList)
         End If
     End Sub
 
